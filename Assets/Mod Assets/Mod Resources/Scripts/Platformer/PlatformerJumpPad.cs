0f7dffa73722836143705eeb6c78fd34a832a22c
Margo Bal <bal.m.v_ipz24@rcit.ukr.education>
2026-09-18 04:05:15 +0300

    Юніт 1: персонаж і рух

--- Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerJumpPad.cs
+++ Assets/Mod Assets/Mod Resources/Scripts/Platformer/PlatformerJumpPad.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using Platformer.Mechanics;
 
 public class PlatformerJumpPad : MonoBehaviour
 {
@@ -9,13 +8,10 @@ public class PlatformerJumpPad : MonoBehaviour
     {
         var rb = other.attachedRigidbody;
         if (rb == null) return;
-        var player = rb.GetComponent<PlayerController>();
-        if (player == null) return;
-        AddVelocity(player);
-    }
 
-    void AddVelocity(PlayerController player)
-    {
-        player.velocity.y = verticalVelocity;
+        var player = rb.GetComponent<Platformer.Mechanics.PlayerController>();
+        if (player == null) return;
+
+        player.Bounce(verticalVelocity);
     }
 }
